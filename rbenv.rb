c5a0ed1effd44a026a4217a746c2fd1501b0f228
Lucas Chan <lucas-github@lucaschan.com>
2011-09-10 23:32:41 +1000

    Initial commit.

---
+++ rbenv.rb
@@ -0,0 +1,13 @@
+dep 'rbenv-install' do
+  met? {
+    shell 'PATH="$HOME/.rbenv/bin:$PATH" rbenv'
+  }
+  meet {
+    cd('~') {
+      log_shell 'Cloning rbenv', "git clone git://github.com/sstephenson/rbenv.git .rbenv"
+      log_shell 'rbenv setup', %Q{echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile}
+      shell %Q{echo 'eval "$(rbenv init -)"' >> .bash_profile}
+      # TODO: figure out why 'exec' borks.
+    }
+  }
+end
