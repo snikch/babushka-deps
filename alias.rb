82de4e5af31e26143abd10f92820093c33b8ed45
Lucas Chan <lucas-github@lucaschan.com>
2011-09-13 22:26:49 +1000

    Added alias for default/compiled ruby. TODO: Make it work for non /usr/local installs.

--- alias.rb
+++ alias.rb
@@ -13,3 +13,19 @@ dep 'alias-bx-rbenv' do
     log "NOTE: Please log out/in or source your ~/.bash_profile"
   }
 end
+
+dep 'alias-bx' do
+  met? {
+    shell? "grep 'function bx()' $HOME/.bash_profile"
+  }
+  meet {
+    cd('~') {
+      log_shell "Creating alias 'bx' for 'bundle exec'",
+        %Q{echo 'function bx() { /usr/local/bin/bundle exec "$@" ;}' >> .bash_profile}
+
+    }
+  }
+  after {
+    log "NOTE: Please log out/in or source your ~/.bash_profile"
+  }
+end
