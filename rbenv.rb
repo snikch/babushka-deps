a797cf311d33b0e1e5700e6c812d928da781feb4
Mal Curtis <mal@sitepoint.com>
2012-06-25 11:19:45 +1200

    Make 1.9.3-p194 the default rbenv ruby version

--- rbenv.rb
+++ rbenv.rb
@@ -1,5 +1,8 @@
 dep 'rbenv' do
-  requires 'rbenv-configured'
+  requires '1.9.3.rbenv'
+  version = '1.9.3-p194'
+  met? { shell "rbenv global | grep #{version}" }
+  meet { log_shell "Setting #{version} as the global rbenv default", "rbenv global #{version}" }
 end
 
 dep 'rbenv-configured' do
@@ -24,4 +27,49 @@ dep 'rbenv-install' do
       # TODO: figure out why 'exec' borks.
     }
   }
+end
+
+meta :rbenv do
+  accepts_value_for :version, :basename
+  accepts_value_for :patchlevel
+  accepts_block_for :customise
+  template {
+    def version_spec
+      "#{version}-#{patchlevel}"
+    end
+    def prefix
+      "~/.rbenv/versions" / version_spec
+    end
+    def version_group
+      version.scan(/^\d\.\d/).first
+    end
+    requires 'rbenv-configured', 'yaml headers.managed'
+    met? {
+      (prefix / 'bin/ruby').executable? and
+      shell(prefix / 'bin/ruby -v')[/^ruby #{version}#{patchlevel}\b/]
+    }
+    meet {
+      yaml_location = shell('brew info libyaml').split("\n").collapse(/\s+\(\d+ files, \S+\)/).first
+      handle_source "http://ftp.ruby-lang.org/pub/ruby/#{version_group}/ruby-#{version_spec}.tar.gz" do |path|
+        invoke(:customise)
+        log_shell 'Configure', "./configure --prefix='#{prefix}' --with-libyaml-dir='#{yaml_location}' CC=/usr/bin/gcc-4.2"
+        log_shell 'Build',     "make -j#{Babushka.host.cpus}"
+        log_shell 'Install',   "make install"
+
+        # ruby-1.9.2 doesn't install bin/* when the build path contains a dot-dir.
+        shell "cp bin/* #{prefix / 'bin'}"
+      end
+    }
+    after {
+      log_shell 'rbenv rehash', 'rbenv rehash'
+    }
+  }
+end
+
+dep '1.9.2.rbenv' do
+  patchlevel 'p290'
+end
+
+dep '1.9.3.rbenv' do
+  patchlevel 'p194'
 end
