cf64abbd3c2bab7b42cba91e19a693d69b92c2e5
Mal Curtis <mal@sitepoint.com>
2012-06-25 11:51:38 +1200

    Added alfred nad total terminal

---
+++ app_bundles.rb
@@ -0,0 +1,27 @@
+dep 'Vim.app' do
+    source 'http://macvim.org/OSX/files/binaries/OSX10_4/Vim7.0-univ.tar.bz2'
+end
+
+dep 'Google Chrome.app' do
+    source 'https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+end
+
+dep 'Dropbox.app' do
+    source 'http://cdn.dropbox.com/Dropbox%201.2.49.dmg'
+end
+
+dep 'Skype.app' do
+    source 'http://download.skype.com/macosx/Skype_2.8.0.851.dmg'
+end
+
+dep 'Firefox.app' do
+    source 'http://download.mozilla.org/?product=firefox-3.6.10&os=osx&lang=en-US'
+end
+
+dep 'TotalTerminal.app' do
+  source 'http://downloads.binaryage.com/TotalTerminal-1.2.3.dmg'
+end
+
+dep 'Alfred.app' do
+  source 'http://rwc.cachefly.net/alfred_1.2_220.dmg'
+end
