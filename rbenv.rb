ba508ebdb99ad2cf7edc5c74bd54ef028c1da4b6
Mal Curtis <mal@sitepoint.com>
2012-06-25 11:12:20 +1200

    Update master

--- rbenv.rb
+++ rbenv.rb
@@ -1,3 +1,19 @@
+dep 'rbenv' do
+  requires 'rbenv-configured'
+end
+
+dep 'rbenv-configured' do
+  requires 'rbenv-install'
+  met? { '$HOME/.bashrc'.p.grep 'rbenv' }
+  meet { 
+    log_shell 'rbenv setup', %Q{echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bashrc}
+    shell %Q{echo 'eval "$(rbenv init -)"' >> .bashrc}
+  }
+  after {
+    log "NOTE: Please log out/in or source your ~/.bashrc"
+  }
+end
+
 dep 'rbenv-install' do
   met? {
     shell? 'PATH="$HOME/.rbenv/bin:$PATH" rbenv'
@@ -5,12 +21,7 @@ dep 'rbenv-install' do
   meet {
     cd('~') {
       log_shell 'Cloning rbenv', "git clone git://github.com/sstephenson/rbenv.git .rbenv"
-      log_shell 'rbenv setup', %Q{echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bashrc}
-      shell %Q{echo 'eval "$(rbenv init -)"' >> .bashrc}
       # TODO: figure out why 'exec' borks.
     }
-  }
-  after {
-    log "NOTE: Please log out/in or source your ~/.bashrc"
   }
 end
