018c130bd700dd57017f1ee2aace70567dc21c42
Mal Curtis <mal@sitepoint.com>
2012-06-26 09:07:38 +1200

    Check for zsh in a different way

--- zsh.rb
+++ zsh.rb
@@ -1,24 +1,24 @@
 dep 'oh-my-zsh' do
-    requires 'zsh as user shell', 'zsh as system shell'
-      def source
-            'https://github.com/robbyrussell/oh-my-zsh.git'
-              end
-        def path
-              File.expand_path '~/.oh-my-zsh'
-                end
-          met? { File.exists?(path) }
-            meet { git source, :to => path }
+  requires 'zsh as user shell', 'zsh as system shell'
+  def source
+    'https://github.com/robbyrussell/oh-my-zsh.git'
+  end
+  def path
+    File.expand_path '~/.oh-my-zsh'
+  end
+  met? { File.exists?(path) }
+  meet { git source, :to => path }
 end
 
 dep 'zsh as user shell' do
-    requires 'zsh.managed'
-      met? { ENV['SHELL'] == which('zsh') }
-        meet { sudo("chsh -s '#{which('zsh')}' #{ENV['USER']}") }
+  requires 'zsh.managed'
+  met? { shell "finger -m `whoami` | grep zsh" }
+  meet { sudo("chsh -s '#{which('zsh')}' #{ENV['USER']}") }
 end
 
 dep 'zsh as system shell' do
-    met? { File.read('/etc/shells').to_s =~ /#{which('zsh')}/ }
-        meet { sudo "echo `which zsh` >> /etc/shells" }
+  met? { File.read('/etc/shells').to_s =~ /#{which('zsh')}/ }
+  meet { sudo "echo `which zsh` >> /etc/shells" }
 end
 
 dep 'zsh.managed'
